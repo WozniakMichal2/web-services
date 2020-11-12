2ae89b7ad33d1f94593dbdd44e8f0100629d71d9
WozniakMichal2 <mwozniak2@edu.cdv.pl>
2020-11-12 12:06:58 +0100

    2

--- express.js
+++ express.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const helmet = require("helmet")
 var bodyParser = require('body-parser')
+let yup = require('yup')
+
 const soapRequest = require('easy-soap-request')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -19,6 +23,39 @@ app.post('/soap', function (req, res) {
 
 app.post('/rest', function (req, res) {
   res.send('Got a POST-REST')
+})
+
+app.get('/info', (req, res) => {
+  var author=require('./package.json')
+  res.json({'author':author.author})
+  res.status(200)
+})
+
+
+
+let schema = yup.object().shape({
+  name: yup.string().trim().max(10).matches(/^[a-zA-Z]+$/).required()
+});
+
+app.get('/hello/:name', (req, res) => {
+  schema.isValid(req.params).then(function (valid) {
+      if (valid) {
+          res.status(200);
+          res.send('Hi: ' + req.params.name);
+      } else {
+          res.send('Error 400');
+          res.status(400);
+      }
+  })
+})
+
+let tab = [];
+app.post('/store', function(req, res) {
+  tab.push(req.body.input)
+  console.log(req.body)
+  res.status(201)
+  res.json({stored_data:tab})
+  
 })
 
 app.use(helmet())
